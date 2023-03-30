e4701685dfc7a3f40ce7e44af8728cdfd1c1c129
Nikolay Shikalenko <wolfox18@gmail.com>
2023-03-31 01:11:33 +0700

    MVP release

--- src/components/Answer/Answer.js
+++ src/components/Answer/Answer.js
@@ -1,19 +1,34 @@
 import React from "react";
 import "./Answer.css";
-import { Variant } from "../Variant/Variant";
 
 export const Answer = ({ rightAnswer, variants }) => {
+  const [selectedVariant, setSelectedVariant] = React.useState(null);
+  React.useEffect(()=>{
+    setSelectedVariant(null)
+  }, variants);
 
   return (
     <section className="block">
       <div className="block__container">
         <h2 className="answer__title">Твой ответ?</h2>
         <ul className="answer__variants">
-          {variants.map((variant) => (
-            <Variant
-              number={variant}
-              isCorrect={variant === rightAnswer ? true : false}
-            />
+          {variants.map((variant, index) => (
+            <li
+            key={index}  
+            onClick={() => {
+              console.log(index);
+                setSelectedVariant(index);
+              }}
+              className={`answer__variant answer__variant_type_${
+                selectedVariant === index
+                  ? variant === rightAnswer
+                    ? "correct"
+                    : "wrong"
+                  : "unselected"
+              }`}
+            >
+              {variant}
+            </li>
           ))}
         </ul>
       </div>
